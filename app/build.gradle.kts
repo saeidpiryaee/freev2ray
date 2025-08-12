72db33f3083e5cc4877afdaff5d98aff10dbfd5a
saeidpiryaee <piryaeesaeid@gmail.com>
2025-08-13 01:10:49 +0330

    Update : build.gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.pinkypromise.v2rayconfig"
         minSdk = 24
         targetSdk = 36
-        versionCode = 13
-        versionName = "2.3"
+        versionCode = 14
+        versionName = "2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
